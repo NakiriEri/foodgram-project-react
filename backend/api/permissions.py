f0425411d0ca1199b7b43a9f0bf5276812d9779c
NakiriEri <50824033+NakiriEri@users.noreply.github.com>
2023-07-14 12:59:07 +0300

    1231

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -4,5 +4,5 @@ from rest_framework import permissions
 class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.method in (
-                permissions.SAFE_METHODS or view.author == request.user
+            permissions.SAFE_METHODS or view.author == request.user
         )
