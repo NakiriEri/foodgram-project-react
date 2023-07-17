d3bd59bf9db2f542e72e6f471f5764a79e8a39a8
NakiriEri <50824033+NakiriEri@users.noreply.github.com>
2023-07-17 17:10:58 +0300

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -21,10 +21,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
-    'rest_framework',
+    'rest_framework''
     'rest_framework.authtoken',
     'djoser',
+    'users',
     'api',
     'recipes',
 ]
