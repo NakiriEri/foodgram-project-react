64acd06669b62d85769573302bfc297843368afc
NakiriEri <50824033+NakiriEri@users.noreply.github.com>
2023-07-17 19:22:10 +0300

    Update import.py

--- backend/recipes/management/commands/import.py
+++ backend/recipes/management/commands/import.py
@@ -10,5 +10,7 @@ with open('recipes/management/commands/ingredients.csv',
           encoding='utf-8') as f:
 reader = csv.reader(f)
 for row in reader:
-_, created = Ingredient.objects.get_or_create(name=row[0], measures=row[1])
+_, created = Ingredient.objects.get_or_create(name=row[0], 
+                                              measures=row[1]
+                                             )
 print("Импорт успешно завершен")
