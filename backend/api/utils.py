a3e2fd3bb5f595655f5c201ec1fa0be096a8c908
NakiriEri <50824033+NakiriEri@users.noreply.github.com>
2023-07-17 19:47:40 +0300

    Update utils.py

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -10,21 +10,17 @@ def add_to(model, request, user, pk):
     if model.objects.filter(user=user, id = pk).exists():
         return Response({'Невозможно добавить, уже существует данная единица'},
                         status=status.HTTP_400_BAD_REQUEST)
-
     recipe = get_object_or_404(Recipe, id=pk)
     instance = model.objects.create(user=user, recipe=recipe)
     serializer = SmallRecipeSerializer(recipe)
-
     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 def delete_from(model, user, pk, request):
     recipe = get_object_or_404(Recipe, id=pk)
-
     if model.objects.filter(user=user, recipe=recipe).exists():
         model.objects.filter(
             user=user, recipe=recipe
         ).delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
     return Response(status=status.HTTP_400_BAD_REQUEST)
