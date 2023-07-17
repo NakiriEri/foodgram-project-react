6b53113161ee472511b9ed0debda8d8e0153239d
Pavel Tugolukov <pahkarus@gmail.com>
2023-07-17 12:43:25 +0000

    update docker

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -5,7 +5,7 @@ from recipes.models import Ingredient, Recipe, Tag
 
 class RecipeFilter(FilterSet):
     tags = filters.ModelMultipleChoiceFilter(
-        queryset=Tag.objects.all()
+        queryset=Tag.objects.all(),
         field_name='tags__slug',
         to_field_name='slug',
     )
