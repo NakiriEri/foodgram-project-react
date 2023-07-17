36eb84aa3af9e25de609ea5a9110493d9cf937bf
NakiriEri <50824033+NakiriEri@users.noreply.github.com>
2023-07-17 19:55:37 +0300

    Update import.py

--- backend/recipes/management/commands/import.py
+++ backend/recipes/management/commands/import.py
@@ -7,13 +7,13 @@ from recipes.models import Ingredient
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         with open(
-            'recipes/management/commands/ingredients.csv', 
-            'r', 
+            'recipes/management/commands/ingredients.csv',
+            'r',
             encoding='utf-8') as f:
             reader = csv.reader(f)
             for row in reader:
                 _, created = Ingredient.objects.get_or_create(
-                    name=row[0], 
+                    name=row[0],
                     measures=row[1]
                 )
         print("Импорт успешно завершен")
