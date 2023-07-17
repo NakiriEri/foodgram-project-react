ac6543328f8ead749dce955f2798a1a4aa622b96
Pavel Tugolukov <pahkarus@gmail.com>
2023-07-17 08:51:41 +0000

    update filter

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -13,27 +13,27 @@ class RecipeFilter(FilterSet):
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         method='filter_is_in_shopping_cart')
+    created = filters.DateFilter(field_name='created', lookup_expr='gte')
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'author',)
+        fields = ('tags', 'author', 'created',)
 
     def filter_is_favorited(self, queryset, name, value):
-        user = self.request.user
-        if value and not user.is_anonymous:
-            return queryset.filter(favorite__user=user)
+        if user.is_authenticated:
+            return queryset.filter(favorite__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
-        user = self.request.user
-        if value and not user.is_anonymous:
-            return queryset.filter(shopping_cart__user=user)
+        if user.is_authenticated:
+            return queryset.filter(favorite__user=self.request.user)
         return queryset
 
 
 class IngredientFilter(FilterSet):
-    name = filters.CharFilter(lookup_expr='istartswith')
+    name = filters.CharFilter(field_name='name', lookup_expr='icontains')
+
 
     class Meta:
         model = Ingredient
-        fields = ('name', )
+        fields = ('name',)
