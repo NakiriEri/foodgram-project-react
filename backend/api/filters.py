9325e64859f538c239f95cb71dc39ff7af56b21e
NakiriEri <50824033+NakiriEri@users.noreply.github.com>
2023-07-17 14:09:23 +0300

    Update filters.py

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -5,29 +5,29 @@ from recipes.models import Ingredient, Recipe, Tag
 
 class RecipeFilter(FilterSet):
     tags = filters.ModelMultipleChoiceFilter(
+        queryset=Tag.objects.all()
         field_name='tags__slug',
         to_field_name='slug',
-        queryset=Tag.objects.all(),
     )
 
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         method='filter_is_in_shopping_cart')
+    created = filters.DateFilter(field_name='created', lookup_expr='gte')
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'author',)
+        fields = ('tags', 'author','created')
 
     def filter_is_favorited(self, queryset, name, value):
-        user = self.request.user
-        if value and not user.is_anonymous:
-            return queryset.filter(favorite__user=user)
+        if value:
+            return queryset.filter(favorite__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         user = self.request.user
-        if value and not user.is_anonymous:
-            return queryset.filter(shopping_cart__user=user)
+        if value:
+            return queryset.filter(shopping_cart__user=self.request.user)
         return queryset
 
 
